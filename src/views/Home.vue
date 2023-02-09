f4c89a4edafd63df7c221d6de2c40a906d179202
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-02-08 21:39:56 -0300

    Retrieving products from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <script setup>
 import Layout from "../components/Layout.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+const store = useStore();
+
+const productsList = computed(() => {
+  return store.state.products;
+});
 </script>
 
 <template>
   <main>
     <Layout>
       <h2 class="text-3xl font-bold underline">Hello</h2>
+      <ul>
+        <li v-for="product in productsList" :key="product.name">
+          {{ product.name }}
+        </li>
+      </ul>
     </Layout>
   </main>
 </template>
